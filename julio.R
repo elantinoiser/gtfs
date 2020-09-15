4fc6a83778c8f848b9a1a6829323b7334f982b6c
elantinoiser <69495824+elantinoiser@users.noreply.github.com>
2020-09-15 17:35:45 -0500

    Update julio.R

--- julio.R
+++ julio.R
@@ -1,5 +1,6 @@
+#install.packages("sf")
 julio <- readr::read_csv("C:/Users/85412/Desktop/gtfs_rt/julio.csv")
-julio$CST6CDT<- lubridate::as_datetime(julio$TIMESTAMP, tz="CST6CDT")
+julio$CST6CDT <- lubridate::as_datetime(julio$TIMESTAMP, tz="CST6CDT")
 colnames(julio)[9] <- "LATITUDE"
 
 
