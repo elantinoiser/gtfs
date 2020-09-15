3de9f010aacc5d6173dfe19ff9e05f41e1c1cf34
elantinoiser <69495824+elantinoiser@users.noreply.github.com>
2020-09-15 18:32:00 -0500

    Update julio.R

--- julio.R
+++ julio.R
@@ -3,6 +3,10 @@ julio <- readr::read_csv("C:/Users/85412/Desktop/gtfs_rt/julio.csv")
 julio$CST6CDT <- lubridate::as_datetime(julio$TIMESTAMP, tz="CST6CDT")
 colnames(julio)[9] <- "LATITUDE"
 julio$LATITUDE <- stringr::str_sub(julio$LATITUDE, 10,-1)
+julio$VEHICLE <- stringr::str_sub(julio$VEHICLE, 4,-1)
+julio$as_date<- lubridate::as_date(julio$CST6CDT)
+
+
 #Cargar el gtfs estático para unirlo con la ruta del Metrobús a la que pertenece
 gtfs_estatico<-sf::st_read("/Users/85412/Desktop/gtfs_estatico/gtfs_estatico.shp")
 gtfs_estatico <- gtfs_estatico %>%  select(agencia, ruta, geometry) %>% filter(agencia == "METROBUS")
